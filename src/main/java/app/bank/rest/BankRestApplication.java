58c2f024be3eea1ae677ea8537df3a16fb086f62
Dario Guangasi <dari_011b@hotmail.com>
2024-10-25 09:27:07 -0500

    first commit

---
+++ src/main/java/app/bank/rest/BankRestApplication.java
@@ -0,0 +1,13 @@
+package app.bank.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app/bank/rest/BankRestApplicationTests.java
@@ -0,0 +1,13 @@
+package app.bank.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
